a5aa76acde0b22efa8a136e2dd57b9b3b4f7321e
German Murillo <germalganisger@gmail.com>
2015-04-09 09:54:25 -0600

    Added border It seem master branch is broken

--- Core/Source/DTAttributedTextCell.m
+++ Core/Source/DTAttributedTextCell.m
@@ -59,7 +59,7 @@
 		CGFloat neededContentHeight = [self requiredRowHeightInTableView:_containingTableView];
 		
 		// after the first call here the content view size is correct
-		CGRect frame = CGRectMake(0, 0, self.contentView.bounds.size.width, neededContentHeight);
+		CGRect frame = CGRectMake(8, 0, self.contentView.bounds.size.width - 16, neededContentHeight);
 		self.attributedTextContextView.frame = frame;
 	}
 }
@@ -127,7 +127,7 @@
 	}
 	
 	BOOL ios6Style = (NSFoundationVersionNumber < DTNSFoundationVersionNumber_iOS_7_0);
-	CGFloat contentWidth = tableView.frame.size.width;
+	CGFloat contentWidth = tableView.frame.size.width - 16;
 	
 	// reduce width for grouped table views
 	if (ios6Style && tableView.style == UITableViewStyleGrouped)
